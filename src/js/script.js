1560286cd2c123cd8f6d3aceed7a671420131af5
Ranis Galiev <galievdesign@gmail.com>
2021-06-08 10:29:07 +0300

    init commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', setVh)
+
+// window.addEventListener('resize', setVh)
+
+function setVh() {
+  document
+    .querySelector(':root')
+    .style.setProperty('--vh', window.innerHeight / 100 + 'px')
+}
