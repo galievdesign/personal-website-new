a509a205ef3e6c47f262f09d0585c91783152c6e
Ranis Galiev <galievdesign@gmail.com>
2021-11-29 14:16:51 +0300

    fixes

---
+++ docs/index.99f5eb1c.js
@@ -0,0 +1,2 @@
+function setVh(){document.querySelector(":root").style.setProperty("--vh",window.innerHeight/100+"px")}document.addEventListener("DOMContentLoaded",setVh);
+//# sourceMappingURL=index.99f5eb1c.js.map
